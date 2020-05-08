66aaff74980df71f96ddaf66085b046531f0478e
Chahat Bhatia <chahatbhatia90@gmail.com>
2020-05-08 11:26:32 +0530

    [ Chahat ] | Added styling to Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import Navbar from "./components/navbar/NavBar";
+import Navbar from "./components/navbar/Navbar";
 
 
 library.add(faShoppingCart);
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from "../../logo.png";
 
 import { Nav , Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, NavLink , Button} from "reactstrap";
 class NavBar extends Component {
@@ -12,26 +13,27 @@ class NavBar extends Component {
     }  
     render() {
     return (
-      <Navbar>
-        <NavbarBrand data-testid="navbar-brand">
-          <img />
+      <Navbar color="light" light expand="md">
+        <NavbarBrand data-testid="navbar-brand" className="navbar-brand ml-5">
+          <img src={logo} alt="logo" style={{ width: 80, height: 60 }}/>
           On Time Food
         </NavbarBrand>
         <NavbarToggler onClick={this.toggle}/>
-        <Collapse isOpen={this.state.isOpen} >
-            <Nav >
-              <NavItem>
+        <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem className="pl-4">
                   <NavLink>Home</NavLink>
               </NavItem>
-              <NavItem>
+              <NavItem className="pl-4">
                   <NavLink>About</NavLink>
               </NavItem>
-              <NavItem>
+              <NavItem className="pl-4">
                   <NavLink>
-                    <FontAwesomeIcon icon="shopping-cart" />
+                    <FontAwesomeIcon icon="shopping-cart" />{" "}
+                    {this.props.itemTotal}
                   </NavLink>
               </NavItem>
-              <a >
+              <a className="pl-4">
                 <Button className="cart-btn">
                   Show Cart
                 </Button>
